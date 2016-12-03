c614687bb417486fd977c605a6f87a8621d9944e
MathewJoseph31 <mathew_joseph31@yahoo.com>
2016-12-03 13:27:31 +0530

    accidental change in Configuration

--- XDataGrading/src/util/Configuration.java
+++ XDataGrading/src/util/Configuration.java
@@ -25,14 +25,10 @@ public class Configuration implements ConfigurationInterface{
 	//public static String assignmentFolder= getProperty("assignmentFolder");
 	public static ConfigurationInterface object;
 	static void getObject(){
-//		try {
-//			object = (ConfigurationInterface) Class.forName("database.Configuration").newInstance();
-//		} catch (Exception e) {
-//			// Ignoring error
-//		}
-		if(object == null){
-			object = new Configuration();
-			InitLogger.initLogger();
+		try {
+			object = (ConfigurationInterface) Class.forName("database.Configuration").newInstance();
+		} catch (Exception e) {
+			// Ignoring error
 		}
 	}
 	
