be7a5fda749d81f4fba47de43cc3ff3770b84e5a
Ananyo Banerjee <ananyo@cse.iitb.ac.in>
2017-07-26 20:44:52 +0530

    bug fix:testcon and dbcon in getMarkDetails

--- XDataGrading/src/instructor/EvaluateQuestion.java
+++ XDataGrading/src/instructor/EvaluateQuestion.java
@@ -56,7 +56,7 @@ public class EvaluateQuestion extends HttpServlet {
 	 */
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		HttpSession session=request.getSession();		
 		String courseID = (String) request.getSession().getAttribute(
 				"context_label");
@@ -65,80 +65,80 @@ public class EvaluateQuestion extends HttpServlet {
 		boolean evaluationCompleted = false;
 		try(Connection dbcon = (new DatabaseConnection()).dbConnection()){
 			String update="select tajudgement from xdata_student_queries where assignment_id = ? and question_id=? and course_id= ? ";
-					try(PreparedStatement updatestmt=dbcon.prepareStatement(update)){
-						updatestmt.setInt(1, Integer.parseInt(assignment_id));
-						updatestmt.setInt(2,Integer.parseInt(question_id));
-						updatestmt.setString(3,courseID);
-						int query_id=1;
-						ResultSet rs =  updatestmt.executeQuery();
-						while(rs.next()){
-							if(rs.getBoolean("tajudgement")){
-								//continue
-								evaluationCompleted = true;
-							}else{
-								evaluationCompleted = false;
-								break;
-							}
-						}
-					}//try block for statement ends
-				} catch (SQLException e1) {
-					logger.log(Level.SEVERE,e1.getMessage(),e1);
-					//throw new ServletException(e1);
+			try(PreparedStatement updatestmt=dbcon.prepareStatement(update)){
+				updatestmt.setInt(1, Integer.parseInt(assignment_id));
+				updatestmt.setInt(2,Integer.parseInt(question_id));
+				updatestmt.setString(3,courseID);
+				int query_id=1;
+				ResultSet rs =  updatestmt.executeQuery();
+				while(rs.next()){
+					if(rs.getBoolean("tajudgement")){
+						//continue
+						evaluationCompleted = true;
+					}else{
+						evaluationCompleted = false;
+						break;
+					}
 				}
+			}//try block for statement ends
+		} catch (SQLException e1) {
+			logger.log(Level.SEVERE,e1.getMessage(),e1);
+			//throw new ServletException(e1);
+		}
 		if(!evaluationCompleted){
-				response.sendError(00,"INCOMPLETE");
+			response.sendError(00,"INCOMPLETE");
 		}else{
 			response.setStatus(200); 
 		}
 	}
-	
+
 	public void evaluateQuestion(String assignment_id, String question_id,String courseID) throws ServletException{
-		
-	
+
+
 		try(Connection dbcon = (new DatabaseConnection()).dbConnection()){
 			String update="update xdata_student_queries set tajudgement = false where assignment_id = ? and question_id=? and course_id= ? ";
-					try(PreparedStatement updatestmt=dbcon.prepareStatement(update)){
-						updatestmt.setInt(1, Integer.parseInt(assignment_id));
-						updatestmt.setInt(2,Integer.parseInt(question_id));
-						updatestmt.setString(3,courseID);
-						int query_id=1;
-						updatestmt.execute();
-					}//try block for statement ends
-				} catch (SQLException e1) {
-					logger.log(Level.SEVERE,e1.getMessage(),e1);
-					//throw new ServletException(e1);
-				}
-		
+			try(PreparedStatement updatestmt=dbcon.prepareStatement(update)){
+				updatestmt.setInt(1, Integer.parseInt(assignment_id));
+				updatestmt.setInt(2,Integer.parseInt(question_id));
+				updatestmt.setString(3,courseID);
+				int query_id=1;
+				updatestmt.execute();
+			}//try block for statement ends
+		} catch (SQLException e1) {
+			logger.log(Level.SEVERE,e1.getMessage(),e1);
+			//throw new ServletException(e1);
+		}
+
 		Connection dbcon = null;
-				try{
-					String s = null; 
-					
-					String args[] = {assignment_id.trim(), question_id.trim(),courseID};
-					TestAssignment.entry(args);
-					
-					
-					dbcon = (new DatabaseConnection()).dbConnection();
-						
-					PreparedStatement updatestmt=dbcon.prepareStatement("update xdata_instructor_query set evaluationstatus = true where assignment_id=? and question_id=? and course_id=?");
-							updatestmt.setInt(1, Integer.parseInt(assignment_id));
-							updatestmt.setInt(2,Integer.parseInt(question_id));
-						
-							updatestmt.setString(3,courseID);
-							updatestmt.execute();			
-					
-				}catch(Exception e){
-					logger.log(Level.SEVERE,e.getMessage(),e);
-					//throw new ServletException(e);
-				}finally{
-					try {
-						if(dbcon != null){
-						dbcon.close();
-						}
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						logger.log(Level.SEVERE,e.getMessage(),e);
-					}
+		try{
+			String s = null; 
+
+			String args[] = {assignment_id.trim(), question_id.trim(),courseID};
+			TestAssignment.entry(args);
+
+
+			dbcon = (new DatabaseConnection()).dbConnection();
+
+			PreparedStatement updatestmt=dbcon.prepareStatement("update xdata_instructor_query set evaluationstatus = true where assignment_id=? and question_id=? and course_id=?");
+			updatestmt.setInt(1, Integer.parseInt(assignment_id));
+			updatestmt.setInt(2,Integer.parseInt(question_id));
+
+			updatestmt.setString(3,courseID);
+			updatestmt.execute();			
+
+		}catch(Exception e){
+			logger.log(Level.SEVERE,e.getMessage(),e);
+			//throw new ServletException(e);
+		}finally{
+			try {
+				if(dbcon != null){
+					dbcon.close();
 				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				logger.log(Level.SEVERE,e.getMessage(),e);
+			}
+		}
 	}
 
 }
