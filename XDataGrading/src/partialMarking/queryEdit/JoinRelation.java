fdb5d3a449642135334a6ecbd4ca86e3229ea1d7
Ananyo Banerjee <ananyo@cse.iitb.ac.in>
2018-01-18 20:11:51 +0530

    Add Join Relation interface

---
+++ XDataGrading/src/partialMarking/queryEdit/JoinRelation.java
@@ -0,0 +1,28 @@
+package partialMarking.queryEdit;
+
+import java.util.List;
+
+import parsing.QueryStructure;
+
+public class JoinRelation implements QueryComponent {
+
+	@Override
+	public List<QueryStructure> edit(QueryStructure student, QueryStructure instructor) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<QueryStructure> add(QueryStructure student, QueryStructure instructor) throws Exception {
+		
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<QueryStructure> remove(QueryStructure student, QueryStructure instructor) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
