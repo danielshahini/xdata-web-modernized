21aab3229b5a17e417f8e926ccd6820df2b48b6f
Ananyo Banerjee <ananyo@cse.iitb.ac.in>
2018-02-07 18:40:56 +0530

    Join Condition Class created

---
+++ XDataGrading/src/partialMarking/queryEdit/JoinCondition.java
@@ -0,0 +1,27 @@
+package partialMarking.queryEdit;
+
+import java.util.List;
+
+import parsing.QueryStructure;
+
+public class JoinCondition implements QueryComponent {
+
+	@Override
+	public List<QueryStructure> edit(QueryStructure student, QueryStructure instructor) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<QueryStructure> add(QueryStructure student, QueryStructure instructor) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<QueryStructure> remove(QueryStructure student, QueryStructure instructor) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
