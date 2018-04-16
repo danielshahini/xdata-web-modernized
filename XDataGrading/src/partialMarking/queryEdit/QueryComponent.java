4e90a1538342aae7a99c1e3ed8e76983ee3115f7
Ananyo Banerjee <ananyo@cse.iitb.ac.in>
2018-04-16 18:45:07 +0530

    Selection multiEdit done

--- XDataGrading/src/partialMarking/queryEdit/QueryComponent.java
+++ XDataGrading/src/partialMarking/queryEdit/QueryComponent.java
@@ -1,11 +1,11 @@
 package partialMarking.queryEdit;
 
 import java.util.List;
-
+import util.Pair;
 import parsing.QueryStructure;
 
 public interface QueryComponent {
-	public List<QueryStructure> edit(QueryStructure student, QueryStructure instructor)throws Exception;
-	public List<QueryStructure> add(QueryStructure student, QueryStructure instructor)throws Exception;
-	public List<QueryStructure> remove(QueryStructure student, QueryStructure instructor)throws Exception;
+	public List<Pair<QueryStructure,Float>> edit(QueryStructure student, QueryStructure instructor)throws Exception;
+	public List<Pair<QueryStructure,Float>> add(QueryStructure student, QueryStructure instructor)throws Exception;
+	public List<Pair<QueryStructure,Float>> remove(QueryStructure student, QueryStructure instructor)throws Exception;
 }
--- XDataGrading/src/partialMarking/queryEdit/Selection.java
+++ XDataGrading/src/partialMarking/queryEdit/Selection.java
@@ -6,12 +6,69 @@ import java.util.List;
 import parsing.Node;
 import parsing.QueryStructure;
 import util.Utilities;
-
+import util.Pair;
 public class Selection implements QueryComponent {
 
-	@Override
-	public List<QueryStructure> edit(QueryStructure student, QueryStructure instructor) throws Exception {
-		List<QueryStructure> a = new ArrayList <QueryStructure>();
+	public static float NodeDiff(Node n1,Node n2)
+	{
+		float total_score=0;
+		if(n1.getOperator().equals(n2.getOperator()))
+			total_score++;		
+
+		//if left node of n1 is a column reference
+		boolean tag=true;
+		if(n1.getLeft().getNodeType().equals(Node.getColRefType())){
+			if(!n2.getLeft().getNodeType().equals(Node.getColRefType()))
+				tag=  false;
+			if(!n1.getLeft().getTable().getTableName().equals(n2.getLeft().getTable().getTableName()))
+				tag=  false;
+
+			if(!n1.getLeft().getTableNameNo().equals(n2.getLeft().getTableNameNo()))
+				tag=  false;
+			if(!n1.getLeft().getColumn().getColumnName().equals(n2.getLeft().getColumn().getColumnName()))
+				tag=  false;
+		}
+		//if left node of n1 is a constant value
+		if(n1.getLeft().getNodeType().equals(Node.getValType())){
+			if(!n2.getLeft().getNodeType().equals(Node.getValType()))
+				tag=   false;
+			if(!n1.getLeft().getStrConst().equals(n2.getLeft().getStrConst()))
+				tag=  false;
+		}
+		if(tag)
+			total_score++;
+		tag=true;
+		if(n1.getRight().getNodeType().equals(Node.getColRefType())){
+
+			if(!n2.getRight().getNodeType().equals(Node.getColRefType()))
+				tag=   false;
+
+			if(!n1.getRight().getTable().getTableName().equals(n2.getRight().getTable().getTableName()))
+				tag=  false;
+
+			if(!n1.getRight().getTableNameNo().equals(n2.getRight().getTableNameNo()))
+				tag=  false;
+
+			if(!n1.getRight().getColumn().getColumnName().equals(n2.getRight().getColumn().getColumnName()))
+				tag=  false;
+		}
+
+		if(n1.getRight().getNodeType().equals(Node.getValType())){
+			if(!n2.getRight().getNodeType().equals(Node.getValType()))
+				tag=   false;
+
+			if(!n1.getRight().getStrConst().equals(n2.getRight().getStrConst()))
+				tag=  false;
+		}
+
+		if(tag)
+			total_score++;
+
+			
+		return (float) total_score;
+	}
+	public List<Pair<QueryStructure,Float>> edit(QueryStructure student, QueryStructure instructor) throws Exception {
+		List<Pair<QueryStructure,Float>> a = new ArrayList <Pair<QueryStructure,Float>>();
 		QueryStructure stu_not_matched = (QueryStructure)Utilities.copy(student);
 		QueryStructure stu_matched = (QueryStructure)Utilities.copy(student);	
 		QueryStructure ins_not_matched = (QueryStructure)Utilities.copy(instructor);
@@ -40,37 +97,47 @@ public class Selection implements QueryComponent {
 				QueryStructure temp = (QueryStructure)Utilities.copy(student);
 				temp.getLstSelectionConditions().remove(st);
 				temp.getLstSelectionConditions().add(t);
-				a.add(temp);
+				Pair<QueryStructure,Float> tempCost= new Pair<QueryStructure,Float> ();
+				tempCost.setFirst(temp);
+				tempCost.setSecond(NodeDiff(st,t));
+				a.add(tempCost);
+				
 			}
 		}
 		return a;
 	}
 
 	@Override
-	public List<QueryStructure> add(QueryStructure student, QueryStructure instructor) throws Exception {
-		List<QueryStructure> a = new ArrayList <QueryStructure>();
+	public List<Pair<QueryStructure,Float>> add(QueryStructure student, QueryStructure instructor) throws Exception {
+		List<Pair<QueryStructure,Float>> a = new ArrayList <Pair<QueryStructure,Float>>();
 		for(Node t:instructor.getLstSelectionConditions())
 		{
 			if(!student.getLstSelectionConditions().contains(t))
 			{
 				QueryStructure temp = (QueryStructure)Utilities.copy(student);
 				temp.getLstSelectionConditions().add(t);
-				a.add(temp);
+				Pair<QueryStructure,Float> tempCost= new Pair<QueryStructure,Float> ();
+				tempCost.setFirst(temp);
+				tempCost.setSecond((float) 3.0);
+				a.add(tempCost);
 			}
 		}
 		return a;
 	}
 
 	@Override
-	public List<QueryStructure> remove(QueryStructure student, QueryStructure instructor) throws Exception {
-		List<QueryStructure> a = new ArrayList <QueryStructure>();
+	public List<Pair<QueryStructure,Float>> remove(QueryStructure student, QueryStructure instructor) throws Exception {
+		List<Pair<QueryStructure,Float>> a = new ArrayList <Pair<QueryStructure,Float>>();
 		for(Node t:student.getLstSelectionConditions())
 		{
 			if(!instructor.getLstSelectionConditions().contains(t))
 			{
 				QueryStructure temp = (QueryStructure)Utilities.copy(student);
 				temp.getLstSelectionConditions().remove(t);
-				a.add(temp);
+				Pair<QueryStructure,Float> tempCost= new Pair<QueryStructure,Float> ();
+				tempCost.setFirst(temp);
+				tempCost.setSecond((float) 3.0);
+				a.add(tempCost);
 			}	
 		}
 		
