49ee11941e78dc47f59da86b3888c6a61332c25e
abhishek95x@gmail.com <abhishek95x@gmail.com>
2021-01-18 12:11:39 +0530

    temporary commit Bug fixes

--- XDataGrading/src/util/Configuration.java
+++ XDataGrading/src/util/Configuration.java
@@ -22,6 +22,7 @@ public class Configuration implements ConfigurationInterface{
 	public static String logFile=getProperty("logFile");
 	public static String logLevel=getProperty("logLevel");
 	
+	public static boolean calledFromApplicationTester = false;
 	
 	//public static String assignmentFolder= getProperty("assignmentFolder");
 	public static ConfigurationInterface object;
